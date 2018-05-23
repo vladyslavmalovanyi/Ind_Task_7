f9b15b22c0d225a2993b8f33a3951c8e970dbb93
vladyslav <vladyslavmalovanyi@gmail.com>
2018-05-23 18:25:42 +0300

    MainWindow is designed MainWindow logic is added Features added:     data can be saved using JSON Serialization

--- Ind_Task_7/MainWindow.xaml.cs
+++ Ind_Task_7/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
 
 namespace Ind_Task_7
 {
@@ -20,9 +22,133 @@ namespace Ind_Task_7
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Person> _people = new List<Person>();
+        private static string _jsonFileName = "db.json";
+
         public MainWindow()
         {
             InitializeComponent();
+            DeserializeDB();
         }
+
+        private void DeserializeDB()
+        {
+            try
+            {
+                if (File.Exists(_jsonFileName))
+                {
+                    _people = JsonConvert.DeserializeObject<List<Person>>(File.ReadAllText(_jsonFileName));
+                }
+
+                if (_people.Count > 0)
+                {
+                    for (int i = 0; i < _people.Count; i++)
+                    {
+                        if(Person.count < _people[i].ID) { Person.count = _people[i].ID; } 
+                    }
+
+                }
+                ListBoxInitialize();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ListBoxInitialize()
+        {
+            try
+            {
+                lvPeople.Items.Clear();
+
+                foreach (Person p in _people)
+                {
+                    lvPeople.Items.Add(p);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+        }
+
+        #region UI Event Methods
+
+        private void ButAddPerson_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbFirstName.Text == String.Empty || tbLastName.Text == String.Empty)
+            {
+                Console.Beep();
+            }
+            else
+            {
+                Person next = new Person(tbFirstName.Text, tbLastName.Text);
+
+                _people.Add(next);
+                lvPeople.Items.Add(next);
+
+                tbFirstName.Text = "First Name";
+                tbLastName.Text = "Last Name";
+            }
+        }
+
+        private void ButRemove_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                for (int i = 0; i < lvPeople.SelectedItems.Count; i++)
+                {
+                    _people.Remove(((Person)lvPeople.SelectedItems[i]));
+                }
+
+                ListBoxInitialize();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Result",
+                MessageBoxButton.OK, MessageBoxImage.Error,
+                MessageBoxResult.OK, MessageBoxOptions.None);
+            }
+        }
+
+        private void ButSort_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (_people.Count > 0)
+                {
+                    _people.Sort();
+                    ListBoxInitialize();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Result",
+                MessageBoxButton.OK, MessageBoxImage.Error,
+                MessageBoxResult.OK, MessageBoxOptions.None);
+            }
+        }
+
+        private void ButSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string serialized = JsonConvert.SerializeObject(_people);
+                File.WriteAllText(_jsonFileName, serialized);
+                MessageBox.Show("Saved successfully", "Result",
+                    MessageBoxButton.OK, MessageBoxImage.Information,
+                    MessageBoxResult.OK, MessageBoxOptions.None);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Result",
+                    MessageBoxButton.OK, MessageBoxImage.Error,
+                    MessageBoxResult.OK, MessageBoxOptions.None);
+            }
+        }
+
+        #endregion
     }
 }
---
+++ Ind_Task_7/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ind_Task_7
+{
+    public class Person : IComparable<Person>
+    {
+        public static int count = 0;
+
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //public Person()
+        //{
+        //    FirstName = "First Name";
+        //    LastName = "Last Name";
+        //    count++;
+        //    ID = count;
+        //}
+
+        public Person(string fName, string lName)
+        {
+            FirstName = fName;
+            LastName = lName;
+            count++;
+            ID = count;
+        }
+
+        public int CompareTo(Person obj)
+        {
+            string this_full_name = LastName.ToLower() + " " + FirstName.ToLower();
+            string obj_full_name = obj.LastName.ToLower() + " " + obj.FirstName.ToLower();
+
+            if (this_full_name.CompareTo(obj_full_name) > 0)
+            {
+                return 1;
+            }
+            else if (this_full_name.CompareTo(obj_full_name) < 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
